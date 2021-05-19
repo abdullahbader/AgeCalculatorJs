1daa1978730d6144fbb0fba5b7dfec593550e19c
abdullahbader <abdallah.b.b96@gmail.com>
2021-05-20 00:38:36 +0300

    finalyyyy done

--- app.js
+++ app.js
@@ -13,18 +13,42 @@
 
 // This functions should check the integrity of the parameters and pass true/false
 function checkParamsFn(year, month, day) {
-  // Write your code here
+  let y = Number.isInteger(year);
+  let m = Number.isInteger(month);
+  let d = Number.isInteger(day);
+  if (y && m && d) {
+    return true;
+  } else return false;
 }
 
-// This functions checks if the person is or above 18 years of age, return true/false
 function checkOverEighteenFn(year, month, day) {
-  // Write your code here
+  let birthday = new Date();
+  let y = birthday.getFullYear();
+  let m = birthday.getMonth() + 1;
+  let d = birthday.getDate();
+ 
+  let ry = y - year;
+  let rm = m - month;
+  console.log(rm);
+  let rd = d - day;
+  if (y - year > 18 && month <= 12 && day <= 31) return true;
+  return false;
+ 
 }
-
 function calculateAgeFn(year, month, day) {
-  // Write your code here
-}
+  let f1 = checkParamsFn(year, month, day);
+  let f2 = checkOverEighteenFn(year, month, day);
 
+  let y = new Date().getFullYear();
+
+  if (f1 && f2) {
+    let age = y - year;
+
+    return age;
+  } else {
+    return "error:";
+  }
+}
 // Look at the naming of the functions. it looks like salwaBaqer, where
 // the first letter of the first word is small, while the first letter of the
 // second word is Capital. This is called "Camel Casing."
